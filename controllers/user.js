d5d9fa342c7ca2b725e0dba233ab340fce70e8e5
Nikita Zarutskiy <Aboba410@yandex.ru>
2022-07-11 22:15:15 +0500

    feat/addenvfile

--- controllers/user.js
+++ controllers/user.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
 // const AuthorizationError = require('../utils/errors/authorization-error');
 const BadRequest = require('../utils/errors/bad-request');
 // const ErrorCreated = require('../utils/errors/error-created');
@@ -21,7 +22,11 @@ module.exports.login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       // создадим токен
-      const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
+      const token = jwt.sign(
+        { _id: user._id },
+        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+        { expiresIn: '7d' },
+      );
 
       // вернём токен
       res.send({ token });
